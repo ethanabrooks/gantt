91d8f19a51aefef6423b1ff08224c8e16ab539f2
Ethan Brooks <ethanabrooks@gmail.com>
2020-10-07 19:56:59 -0400

    DARPA version

--- plot.py
+++ plot.py
@@ -11,41 +11,59 @@ label_width = 450
 day0 = date(2020, 7, 1)
 
 file_names = [
-    "controller-metrics.csv",
     "perception.csv",
     "rl.csv",
+    "controller-metrics.csv",
 ]
 
 colors = [
-    "c",
-    "m",
-    "y",
+    "darkgreen",
+    "darkblue",
+    "darkviolet",
 ]
+
+
+def subgroups_gen():
+    for file_name in file_names:
+        with Path("L2M Gantt - " + file_name).open() as f:
+            reader = csv.reader(f)
+            yield next(reader)[0]
+
+
+subgroups = list(subgroups_gen())
 
 
 def to_date(string):
     return date(*map(int, string.split("-")))
 
 
-def values_generator():
-    for file_name, color in zip(file_names, colors):
-        with Path("L2M Gantt - " + file_name).open() as f:
-            reader = csv.reader(f)
-            subgroup = next(reader)[0]
-            next(reader)  # header
-            for row in reader:
-                label, start, end = row
-                start = to_date(start)
-                end = to_date(end)
-                yield label, start, end, subgroup, color
+def file_values(file_name):
+    with Path("L2M Gantt - " + file_name).open() as f:
+        reader = csv.reader(f)
+        next(reader)
+        next(reader)  # header
+        for row in reader:
+            label, start, end = row
+            label = label.strip(".") + "."
+            start = to_date(start)
+            end = to_date(end)
+            yield label, start, end
 
 
-values = sorted(values_generator(), key=lambda t: t[1], reverse=True)
+def gen():
+    for file_name, subgroup, color in zip(file_names, subgroups, colors):
+        for value in sorted(file_values(file_name), key=lambda t: t[1], reverse=True):
+            yield tuple((*value, color))
+        yield subgroup, None, None, color
+
+
+values = list(gen())
 
 
 def days_elapsed():
-    for (label, start, end, _, _) in values:
-        yield (end - day0).days
+    for (label, start, end, *_) in values:
+        if end is not None:
+            yield (end - day0).days
 
 
 total_width = label_width + max(days_elapsed())
@@ -57,32 +75,47 @@ def plot(highlight, out_name=None):
     fig, ax = plt.subplots(figsize=(w, h))
     ax.set_xlim(0, total_width)
 
-    for y, (label, start, end, subgroup, color) in enumerate(values):
-        alpha = 1 if color in highlight else 0.2
-        width = (end - start).days
-        left = label_width + (start - day0).days
-        ax.text(
-            x=0,
-            y=y,
-            s=" " + label,
-            ha="left",
-            wrap=True,
-            alpha=alpha,
-            fontsize=fontsize,
-            va="center",
-        )
+    for y, (label, start, end, color) in enumerate(values):
         ax.barh(
             y=y, width=total_width, height=1, left=0, color="grey", alpha=(y % 2) * 0.1
         )
-        ax.barh(
-            y=y,
-            width=width,
-            height=0.7,
-            left=left,
-            color=color,
-            label=subgroup,
-            alpha=alpha,
-        )
+        alpha = 1 if color in highlight else 0.2
+        if None in (start, end):
+            assert start == end
+            ax.text(
+                x=0,
+                y=y,
+                s=" " + label,
+                ha="left",
+                wrap=True,
+                alpha=alpha,
+                fontsize=fontsize,
+                fontweight='bold',
+                color="black",
+                va="center",
+            )
+        else:
+            width = (end - start).days
+            left = label_width + (start - day0).days
+            ax.text(
+                x=0,
+                y=y,
+                s=" " + label,
+                ha="left",
+                wrap=True,
+                alpha=alpha,
+                fontsize=fontsize,
+                color=color,
+                va="center",
+            )
+            ax.barh(
+                y=y,
+                width=width,
+                height=0.7,
+                left=left,
+                color=color,
+                alpha=alpha,
+            )
 
     x = label_width + (datetime.now().date() - day0).days
     ax.vlines(x, ymin=-1, ymax=ymax, color="k")
@@ -95,11 +128,6 @@ def plot(highlight, out_name=None):
         ax.text(x=x, y=len(values), s=f"M{i}", fontsize=fontsize)
         ax.text(x=x, y=1 + len(values), s=month.strftime("%b"), fontsize=fontsize)
 
-    handles, labels = ax.get_legend_handles_labels()
-    indexes = [labels.index(l) for l in set(labels)]
-    handles = [handles[i] for i in indexes]
-    labels = [labels[i] for i in indexes]
-    ax.legend(handles, labels, loc="right", fontsize=fontsize)
     ax.axis("off")
     print(out_name, highlight)
     plt.tight_layout()
@@ -110,11 +138,11 @@ def plot(highlight, out_name=None):
     plt.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for args in [
-        ("c", "controller"),
-        ("m", "perception"),
-        ("y", "rl"),
-        ("cmy", "full"),
+        (["darkgreen"], "controller"),
+        (["darkblue"], "perception"),
+        (["darkviolet"], "rl"),
+        (["darkgreen", "darkblue", "darkviolet"], "full"),
     ]:
         plot(*args)
