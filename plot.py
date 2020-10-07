1ac2268a54215ef300c6ebf9541bf7b7bb4e9afb
Ethan Brooks <ethanabrooks@gmail.com>
2020-10-07 16:55:53 -0400

    done

--- plot.py
+++ plot.py
@@ -1,4 +1,3 @@
-
 from dateutil.relativedelta import relativedelta
 import csv
 from datetime import date, datetime
@@ -7,31 +6,30 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 h = 20
-w = 20
-label_width = 300
+w = 25
+label_width = 450
 day0 = date(2020, 7, 1)
 
 file_names = [
-    'controller-metrics.csv',
-    'perception.csv',
-    'rl.csv',
+    "controller-metrics.csv",
+    "perception.csv",
+    "rl.csv",
 ]
 
-
 colors = [
-    'c',
-    'm',
-    'y',
+    "c",
+    "m",
+    "y",
 ]
 
 
 def to_date(string):
-    return date(*map(int, string.split('-')))
+    return date(*map(int, string.split("-")))
 
 
 def values_generator():
     for file_name, color in zip(file_names, colors):
-        with Path('L2M Gantt - ' + file_name).open() as f:
+        with Path("L2M Gantt - " + file_name).open() as f:
             reader = csv.reader(f)
             subgroup = next(reader)[0]
             next(reader)  # header
@@ -41,53 +39,82 @@ def values_generator():
                 end = to_date(end)
                 yield label, start, end, subgroup, color
 
+
 values = sorted(values_generator(), key=lambda t: t[1], reverse=True)
-# y_step = 1 / len(values)
+
 
 def days_elapsed():
     for (label, start, end, _, _) in values:
         yield (end - day0).days
 
+
 total_width = label_width + max(days_elapsed())
 ymax = len(values) - 1
-x = label_width + (datetime.now().date() - day0).days
-
-for highlighted, out_name in [
-    ('c', 'controller'),
-    ('r', 'perception'),
-    ('y', 'rl'),
-    ('cmy', 'full'),
-]:
+fontsize = 16
 
 
-    fig, ax = plt.subplots(figsize=(h, w))
+def plot(highlight, out_name=None):
+    fig, ax = plt.subplots(figsize=(w, h))
     ax.set_xlim(0, total_width)
 
     for y, (label, start, end, subgroup, color) in enumerate(values):
-        alpha = 1 if color in highlighted else .2
+        alpha = 1 if color in highlight else 0.2
         width = (end - start).days
         left = label_width + (start - day0).days
-        ax.text(x=0, y=y, s=' ' + label, ha='left', wrap=True, alpha=alpha)
-        ax.barh(y=y, width=total_width, height=1, left=0, color='grey', alpha=(y%2) * .1)
-        ax.barh(y=y, width=width, height=.7, left=left, color=color, label=subgroup, alpha=alpha)
+        ax.text(
+            x=0,
+            y=y,
+            s=" " + label,
+            ha="left",
+            wrap=True,
+            alpha=alpha,
+            fontsize=fontsize,
+            va="center",
+        )
+        ax.barh(
+            y=y, width=total_width, height=1, left=0, color="grey", alpha=(y % 2) * 0.1
+        )
+        ax.barh(
+            y=y,
+            width=width,
+            height=0.7,
+            left=left,
+            color=color,
+            label=subgroup,
+            alpha=alpha,
+        )
 
-    ax.vlines(x, ymin=-1, ymax=ymax, color='k')
+    x = label_width + (datetime.now().date() - day0).days
+    ax.vlines(x, ymin=-1, ymax=ymax, color="k")
 
     for i in range(16):
         month = day0 + relativedelta(months=i)
         days = (month - day0).days
         x = label_width + days
-        ax.axvline(x, color='grey', linestyle=':')
-        ax.text(x=x, y=len(values), s=f'M{i}')
-        ax.text(x=x, y=1 + len(values), s=month.strftime("%b"))
-
+        ax.axvline(x, color="grey", linestyle=":")
+        ax.text(x=x, y=len(values), s=f"M{i}", fontsize=fontsize)
+        ax.text(x=x, y=1 + len(values), s=month.strftime("%b"), fontsize=fontsize)
 
     handles, labels = ax.get_legend_handles_labels()
     indexes = [labels.index(l) for l in set(labels)]
     handles = [handles[i] for i in indexes]
     labels = [labels[i] for i in indexes]
-    ax.legend(handles, labels, loc='right', fontsize='large')
-    ax.axis('off')
-    print(out_name, highlighted)
-    plt.savefig(out_name)
+    ax.legend(handles, labels, loc="right", fontsize=fontsize)
+    ax.axis("off")
+    print(out_name, highlight)
+    plt.tight_layout()
+    if out_name:
+        plt.savefig(out_name)
+    else:
+        plt.show()
     plt.close()
+
+
+if __name__ == '__main__':
+    for args in [
+        ("c", "controller"),
+        ("m", "perception"),
+        ("y", "rl"),
+        ("cmy", "full"),
+    ]:
+        plot(*args)
